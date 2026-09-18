a729a44b22b04001dd2449a02a733a438737a09c
dmitriychernenko <cherichtest@gmail.com>
2026-09-18 13:03:57 +0300

    Изменения для ветки A

--- sharp/Test.cs
+++ sharp/Test.cs
@@ -1,15 +1,1 @@
-namespace sharp;
-
-public class Test
-{
-    public static void Main(string[] args)
-    {
-        Console.Write("Enter a number: ");
-        int number = Convert.ToInt32(Console.ReadLine());
-        
-        Console.Write("Enter a number №2: ");
-        int number2 = Convert.ToInt32(Console.ReadLine());
-        
-        Console.Write("Умножение: " + number * number2);
-    }
-}
+// Версия ветки A
